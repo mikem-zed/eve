9a25e9874e28ce0db288ebbe681a5977f5a05744
Mikhail Malyshev <mikem@zededa.com>
2023-04-27 14:40:44 +0000

    Create EVE custom TPM event log
    Signed-off-by: Mikhail Malyshev <mikem@zededa.com>

--- pkg/measure-config/go.mod
+++ pkg/measure-config/go.mod
@@ -2,10 +2,17 @@ module github.com/lf-edge/eve/pkg/measure-config
 
 go 1.20
 
-replace github.com/lf-edge/eve/api/go => github.com/lf-edge/eve/api/go v0.0.0-20221108162354-969f3cba94d8
+require (
+	github.com/canonical/tcglog-parser v0.0.0-20221030122402-0bee1fbba1d9
+	github.com/google/go-tpm v0.3.3
+)
 
-replace github.com/lf-edge/eve/libs/depgraph => github.com/lf-edge/eve/libs/depgraph v0.0.0-20221108162354-969f3cba94d8
+require (
+	github.com/canonical/go-efilib v0.3.1-0.20220314143719-95d50e8afc82 // indirect
+	github.com/canonical/go-sp800.108-kdf v0.0.0-20210315104021-ead800bbf9a0 // indirect
+	github.com/canonical/go-tpm2 v0.1.0 // indirect
+	golang.org/x/sys v0.0.0-20210908233432-aa78b53d3365 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
 
-require github.com/google/go-tpm v0.3.3
-
-require golang.org/x/sys v0.1.0 // indirect
+replace github.com/canonical/tcglog-parser v0.0.0-20221030122402-0bee1fbba1d9 => github.com/mikem-zed/tcglog-parser v0.0.0-20230413004639-7ae5145e8511
--- pkg/measure-config/src/measurefs.go
+++ pkg/measure-config/src/measurefs.go
@@ -11,10 +11,13 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"os"
 	"path/filepath"
 	"sort"
 
+	tpm2log "github.com/canonical/go-tpm2"
+	"github.com/canonical/tcglog-parser"
 	"github.com/google/go-tpm/tpm2"
 	"github.com/google/go-tpm/tpmutil"
 )
@@ -22,6 +25,8 @@ import (
 const (
 	//TpmDevicePath is the TPM device file path
 	TpmDevicePath   = "/dev/tpmrm0"
+	rootfsPCRIndex  = 13
+	rootfsPCRHandle = tpmutil.Handle(tpm2.PCRFirst + rootfsPCRIndex)
 	configPCRIndex  = 14
 	configPCRHandle = tpmutil.Handle(tpm2.PCRFirst + configPCRIndex)
 	//PCREvent (TPM2_PCR_Event) supports event size of maximum 1024 bytes.
@@ -34,8 +39,40 @@ type fileInfo struct {
 }
 
 type tpmEvent struct {
-	data string
-	pcr  []byte
+	rawEventData
+	data   string
+	pcrMap map[tpm2log.HashAlgorithmId][]byte
+	pcr    tcglog.PCRIndex
+}
+
+type rawEventData []byte
+
+func (b rawEventData) Bytes() []byte {
+	return []byte(b)
+}
+
+// implementing the EventData interface
+func (e *tpmEvent) String() string {
+	return fmt.Sprintf("%s", e.data)
+}
+
+func (e *tpmEvent) Write(w io.Writer) error {
+	_, err := io.WriteString(w, fmt.Sprintf("%s\x00", e.data))
+	return err
+}
+
+func (e *tpmEvent) ToTCGEvent() *tcglog.Event {
+	digests := make(tcglog.DigestMap)
+	for alg, digest := range e.pcrMap {
+		digests[alg] = digest
+	}
+	event := tcglog.Event{
+		PCRIndex:  e.pcr,
+		EventType: tcglog.EventTypeIPL,
+		Digests:   digests,
+		Data:      e,
+	}
+	return &event
 }
 
 func min(a, b int) int {
@@ -93,7 +130,7 @@ func sha256sumForFile(filePath string) (string, error) {
 	return hex.EncodeToString(hash.Sum(nil)), nil
 }
 
-func performMeasurement(filePath string, tpm io.ReadWriter, exist bool, content bool) (*tpmEvent, error) {
+func performMeasurement(filePath string, tpm io.ReadWriter, exist bool, content bool, algos []tpm2log.HashAlgorithmId) (*tpmEvent, error) {
 	var eventData string
 	if content {
 		hash, err := sha256sumForFile(filePath)
@@ -117,12 +154,12 @@ func performMeasurement(filePath string, tpm io.ReadWriter, exist bool, content 
 		}
 	}
 
-	pcr, err := readConfigPCR(tpm)
+	pcr, err := readEvePCR(tpm, configPCRIndex, algos)
 	if err != nil {
 		return nil, fmt.Errorf("cannot measure %s. couldn't read PCR: %v", filePath, err)
 	}
 
-	return &tpmEvent{eventData, pcr}, nil
+	return &tpmEvent{data: eventData, pcrMap: pcr}, nil
 }
 
 func getFileMap() (map[string]fileInfo, error) {
@@ -173,12 +210,78 @@ func getSortedFileList(files map[string]fileInfo) []string {
 	return keys
 }
 
+// allocatedPCRBanks returns a list of selections corresponding to the TPM's implemented PCRs.
+func allocatedPCRBanks(rw io.ReadWriter) ([]tpm2.PCRSelection, error) {
+	caps, moreData, err := tpm2.GetCapability(rw, tpm2.CapabilityPCRs, math.MaxUint32, 0)
+	if err != nil {
+		return nil, fmt.Errorf("listing implemented PCR banks: %w", err)
+	}
+	if moreData {
+		return nil, fmt.Errorf("extra data from GetCapability")
+	}
+	var sels []tpm2.PCRSelection
+	for _, cap := range caps {
+		sel, ok := cap.(tpm2.PCRSelection)
+		if !ok {
+			return nil, fmt.Errorf("unexpected data from GetCapability")
+		}
+		// skip empty (unallocated) PCR selections
+		if len(sel.PCRs) == 0 {
+			continue
+		}
+		sels = append(sels, sel)
+	}
+	return sels, nil
+}
+
+func createTpmEventLog(tpm io.ReadWriter) (*tcglog.Log, []tpm2log.HashAlgorithmId, error) {
+
+	var algos []tpm2log.HashAlgorithmId
+	var digestSizes []tcglog.EFISpecIdEventAlgorithmSize
+	pcrSelection, err := allocatedPCRBanks(tpm)
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("cannot create TPM event log (couldn't read enabled PCRs): %v", err)
+	}
+
+	for _, alg := range pcrSelection {
+		algId := tpm2log.HashAlgorithmId(alg.Hash)
+		algos = append(algos, algId)
+		digestSizes = append(digestSizes, tcglog.EFISpecIdEventAlgorithmSize{
+			AlgorithmId: algId,
+			DigestSize:  uint16(algId.Size()),
+		})
+	}
+
+	event := tcglog.Event{
+		PCRIndex:  0,
+		EventType: tcglog.EventTypeNoAction,
+		// this event always has only SHA1 digest for backwards compatibility with TPM 1.2
+		Digests: tcglog.DigestMap{tpm2log.HashAlgorithmSHA1: make(tcglog.Digest, tpm2log.HashAlgorithmSHA1.Size())},
+		Data: &tcglog.SpecIdEvent03{
+			SpecVersionMajor: 2,
+			UintnSize:        2, //FIXME: is this the size of the following array?
+			DigestSizes:      digestSizes}}
+
+	tcg_log, _ := tcglog.NewLog(&event)
+	return tcg_log, algos, nil
+}
+
 func measureConfig(tpm io.ReadWriter) error {
 	files, err := getFileMap()
 
 	if err != nil {
 		return fmt.Errorf("cannot get file list: %v", err)
 	}
+
+	eventLog, digests, err := createTpmEventLog(tpm)
+
+	if err != nil {
+		return fmt.Errorf("cannot create TPM event log: %v", err)
+	}
+
+	//TODO: read root fs measurements and create TPM event
+	//readEvePCR(tpm, rootfsPCRIndex, digests)
 
 	//get sorted list of files. We must always go the same order
 	//otherwise we'll get different PCR value even with exactly the same
@@ -191,12 +294,12 @@ func measureConfig(tpm io.ReadWriter) error {
 
 		if info.exist {
 			if info.measureContent {
-				event, err = performMeasurement(file, tpm, true, true)
+				event, err = performMeasurement(file, tpm, true, true, digests)
 			} else {
-				event, err = performMeasurement(file, tpm, true, false)
+				event, err = performMeasurement(file, tpm, true, false, digests)
 			}
 		} else {
-			event, err = performMeasurement(file, tpm, false, false)
+			event, err = performMeasurement(file, tpm, false, false, digests)
 		}
 		if err != nil {
 			return fmt.Errorf("cannot measure %s: %v", file, err)
@@ -206,18 +309,44 @@ func measureConfig(tpm io.ReadWriter) error {
 		// make sure to break it into 1024 bytes chunks with added indicators
 		// (e.g. part n of m) to be able to reconstruct the even data for validation.
 		// for now we just print our measurements to boot log.
-		log.Printf("%s pcr:%s", event.data, hex.EncodeToString(event.pcr))
+		for i, alg := range event.pcrMap {
+			log.Printf("%s pcr: %d %s", event.data, i, hex.EncodeToString(alg))
+		}
+		eventLog.Events = append(eventLog.Events, event.ToTCGEvent())
 	}
+
+	fOut, err := os.Create("/run/measurefs/tpm_log.bin")
+	if err != nil {
+		return fmt.Errorf("cannot create EVE TPM log file: %v", err)
+	}
+	defer fOut.Close()
+
+	err = eventLog.Write(fOut)
+
+	if err != nil {
+		return fmt.Errorf("cannot write log to EVE TPM log file: %v", err)
+	}
+
 	return nil
 }
 
-func readConfigPCR(tpm io.ReadWriter) ([]byte, error) {
-	pcr, err := tpm2.ReadPCR(tpm, configPCRIndex, tpm2.AlgSHA256)
+func readEvePCR(tpm io.ReadWriter, pcr int, algos []tpm2log.HashAlgorithmId) (map[tpm2log.HashAlgorithmId][]byte, error) {
 
-	if err != nil {
-		return nil, fmt.Errorf("cannot read PCR %d: %v", configPCRIndex, err)
+	pcrMap := make(map[tpm2log.HashAlgorithmId][]byte)
+
+	for _, alg := range algos {
+		algId, err := tpm2.HashToAlgorithm(alg.GetHash())
+		if err != nil {
+			return nil, fmt.Errorf("cannot convert hash to algorithm: %v", err)
+		}
+		pcrVal, err := tpm2.ReadPCR(tpm, pcr, algId)
+		if err != nil {
+			return nil, fmt.Errorf("cannot read PCR %d, alg %d : %v", pcr, alg, err)
+		}
+		pcrMap[alg] = pcrVal
 	}
-	return pcr, nil
+
+	return pcrMap, nil
 }
 
 // Some file like generated certificates do not exist during the installation.
